70c92cb1bfa3b8e6cf0ba7abad934b546eb746ab
Snakevil Zen <zsnakevil@gmail.com>
2017-03-23 17:26:43 +0800

    定义系列索引节点页面组件。

---
+++ lib/view/series.lua
@@ -0,0 +1,9 @@
+require 'class'
+
+--- 系列索引节点页面组件
+-- @module view/series
+-- @author Snakevil Zen <zsnakevil@gmail.com>
+-- @type View.Series
+local page = class'View.Series':extends'View.Node'
+
+return page
