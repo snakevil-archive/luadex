30e0948208a9736c459946ba3000c8d21365412b
Snakevil Zen <zsnakevil@gmail.com>
2017-03-23 20:44:33 +0800

    定义影片索引节点和演员节点的页面组件。

---
+++ lib/view/actor.lua
@@ -0,0 +1,9 @@
+require 'class'
+
+--- 演员节点页面组件
+-- @module view/actor
+-- @author Snakevil Zen <zsnakevil@gmail.com>
+-- @type View.Actor
+local page = class'View.Actor':extends'View.Node'
+
+return page
---
+++ lib/view/list.lua
@@ -0,0 +1,9 @@
+require 'class'
+
+--- 影片索引页面组件
+-- @module view/list
+-- @author Snakevil Zen <zsnakevil@gmail.com>
+-- @type View.List
+local page = class'View.List':extends'View.Node'
+
+return page
