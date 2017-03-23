0b319b1f933a72b57bf0b405d98f0260db7b698c
Snakevil Zen <zsnakevil@gmail.com>
2017-03-23 17:28:22 +0800

    定义影片节点页面组件。

---
+++ lib/view/movie.lua
@@ -0,0 +1,9 @@
+require 'class'
+
+--- 影片节点页面组件
+-- @module view/movie
+-- @author Snakevil Zen <zsnakevil@gmail.com>
+-- @type View.Movie
+local page = class'View.Movie':extends'View.Node'
+
+return page
