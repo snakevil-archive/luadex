411f2697a4c307077c2053a278c37c079b15d9f4
Snakevil Zen <zsnakevil@gmail.com>
2017-03-23 17:27:44 +0800

    定义演员索引节点页面组件。

---
+++ lib/view/actors.lua
@@ -0,0 +1,9 @@
+require 'class'
+
+--- 演员索引节点页面组件
+-- @module view/actors
+-- @author Snakevil Zen <zsnakevil@gmail.com>
+-- @type View.Actors
+local page = class'View.Actors':extends'View.Node'
+
+return page
