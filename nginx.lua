7a5c0536976bc1d1170865afe6393ca223c4cd22
Snakevil Zen <zsnakevil@gmail.com>
2017-03-23 18:00:39 +0800

    修正 ngx_lua 配置有误时响应内容。

--- nginx.lua
+++ nginx.lua
@@ -1,8 +1,9 @@
 if not ngx.var.luadex then
-    ngx.say'$luadex should be set.'
-    ngx.exit(ngx.ERROR)
+    ngx.status = ngx.HTTP_SERVICE_UNAVAILABLE
+    ngx.say'$luadex not set.'
+    ngx.exit(ngx.OK)
 end
 
-package.path = nginx.var.luadex .. '/lib/?.lua;' .. package.path
+package.path = ngx.var.luadex .. '/lib/?.lua;' .. package.path
 
 ngx.say(require'dispatch'(ngx.var.request_filename, ngx.var.request_uri))
