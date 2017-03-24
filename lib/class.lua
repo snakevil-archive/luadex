47003bb2acd0313657b522f99c6db44d6380fa14
Snakevil Zen <zsnakevil@gmail.com>
2017-03-24 10:59:00 +0800

    修正类机制字符串类型转化重载错误地问题。

--- lib/class.lua
+++ lib/class.lua
@@ -8,36 +8,36 @@ class = setmetatable({
 }, {
     __call = function ( self, name )
         local derived = setmetatable({
-            --- 字符串类型转化机制
-            -- 至 LuaJIT-2.0.4 为止，`tostring()` 函数都只会检查并调用**元表**的 `__tostring` 函数。
-            --
-            -- ```
-            -- print(setmetatable({
-            --     __tostring = function ( self )
-            --         return '3'
-            --     end
-            -- }, setmetatable({
-            --     __tostring = function ( self )
-            --         return '2'
-            --     end
-            -- }, {
-            --     __tostring = function ( self )
-            --         return '1'
-            --     end
-            -- })))
-            -- ```
-            --
-            -- 在上面的案例中，输出内容是 `2`。如果将该函数定义删除，输出内容就变成了 `table: 0x01c773b8` 这样的值。
-            --
-            -- 转以面向对象的思维来理解，`3` 是类的实例，`2` 是类，`1` 是父类。
-            --
-            -- 因此为了能够使用本基类统一的 `__tostring` 方法，在定义每个派生类时，都需要显性地定义派生类中的方法，使其能逐层递归调用至基类。
-            __tostring = function ( self )
-                return class.__tostring(self)
-            end,
             __class = name
         }, self)
         derived.__index = derived
+        --- 字符串类型转化机制
+        -- 至 LuaJIT-2.0.4 为止，`tostring()` 函数都只会检查并调用**元表**的 `__tostring` 函数。
+        --
+        -- ```
+        -- print(setmetatable({
+        --     __tostring = function ( self )
+        --         return '3'
+        --     end
+        -- }, setmetatable({
+        --     __tostring = function ( self )
+        --         return '2'
+        --     end
+        -- }, {
+        --     __tostring = function ( self )
+        --         return '1'
+        --     end
+        -- })))
+        -- ```
+        --
+        -- 在上面的案例中，输出内容是 `2`。如果将该函数定义删除，输出内容就变成了 `table: 0x01c773b8` 这样的值。
+        --
+        -- 转以面向对象的思维来理解，`3` 是类的实例，`2` 是类，`1` 是父类。
+        --
+        -- 因此为了能够使用本基类统一的 `__tostring` 方法，在定义每个派生类时，都需要显性地定义派生类中的方法，使其能逐层递归调用至基类。
+        function derived.__tostring ( self )
+            return derived:super().__tostring(self)
+        end
         return derived
     end
 })
